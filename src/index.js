2c5f54991dacd6fbfa64acaa69b684fb81ce75c2
Roman Prytulenko <r.prytulenko@intetics.com>
2020-08-08 11:24:00 +0300

    Add base structure of classes.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './scss/index.scss'
+import {Excel} from '@/components/excel/Excel'
 
-console.log('Hello my web excel!');
+const excel = new Excel('#app', {
+  components: [
+
+  ]
+})
+console.log('Excel: ', excel)
