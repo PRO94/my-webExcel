cb7edd646061e025fa098d05dfe925e1e758cef1
Roman Prytulenko <r.prytulenko@intetics.com>
2020-08-09 08:56:34 +0300

    Added implementation for table component.

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,34 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from './table.template'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   toHTML() {
-    return `
-      <div class="row">
-        <div class="row-info"></div>
-        <div class="row-data">
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-        </div>
-      </div>
-      <div class="row">
-          <div class="row-info">1</div>
-          <div class="row-data">
-              <div class="cell" contenteditable>A111</div>
-              <div class="cell" contenteditable>B111</div>
-              <div class="cell" contenteditable>C111</div>
-          </div>
-      </div>
-      <div class="row">
-          <div class="row-info">2</div>
-          <div class="row-data">
-              <div class="cell" contenteditable>A222</div>
-              <div class="cell" contenteditable>B222</div>
-              <div class="cell" contenteditable>C222</div>
-          </div>
-      </div>
-    `
+    return createTable(25)
   }
 }
