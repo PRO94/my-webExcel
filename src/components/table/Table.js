c2d171e5cb3e898e39a8a72ffdba9884d7c7e2de
Roman Prytulenko <roman.prytulenko@gmail.com>
2020-08-27 07:21:23 +0300

    Added resize functionality (bad performance) for excel table.

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,46 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from './table.template'
+import {createTable} from '@/components/table/table.template'
+import {$} from '@core/dom'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
-    return createTable(25)
+    return createTable(20)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+      const type = $resizer.data.resize
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
+      document.onmousemove = e => {
+        if (type === 'col') {
+          const delta = e.pageX - coords.right
+          const value = coords.width + delta
+          $parent.$el.style.width = value + 'px'
+          $parent.css({width: value + 'px'})
+          cells.forEach(el => el.style.width = value + 'px')
+        } else if (type === 'row') {
+          const delta = e.pageY - coords.bottom
+          const value = coords.height + delta
+          $parent.css({height: value + 'px'})
+        }
+      }
+
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
+    }
   }
 }
