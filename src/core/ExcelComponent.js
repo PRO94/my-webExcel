dbe5621267d8dbbb29ca760fbdb90bb42c09d034
Roman Prytulenko <roman.prytulenko@gmail.com>
2020-08-29 16:05:05 +0300

    Added Facade pattern for using emitter. Implemented table interactivity, 2-way-binding. Added text() to dom.

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,22 +4,42 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
 
     this.prepare()
   }
 
-  // Returns template of component
-  toHTML() {
-    return '';
-  }
-
+  // Setup our component before init
   prepare() { }
 
+  // Initialize component
+  // Add DOM listeners
   init() {
     this.initDOMListeners()
   }
 
+  // Notify listeners about event
+  // Facade pattern: for using this.emitter from Framework
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Subscibe on event
+  $on(event, fn) {
+    const unSub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unSub)
+  }
+
+  // Return template of component
+  toHTML() {
+    return ''
+  }
+
+  // Destroy component
+  // Remove listeners
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
